3e2c0729e9bbe9c1c7e6cdbb17e873029164f783
Sino <ilyas.alfan.b@gmail.com>
2016-10-06 20:41:43 +0200

    Completed IsoscelesTriangle. Rewriting Circle

--- src/graded/figures/Circle.py
+++ src/graded/figures/Circle.py
@@ -1,22 +1,10 @@
 import math
 
 def circle(radius):
-    width = radius ** 2
-    height = radius ** 2
-
     output = ""
 
-    for y in range(height):
-        for x in range(width):
-            a = (x - radius) ** 2
-            b = (y - radius) ** 2
+    # TODO
 
-            z = math.sqrt(a + b)
-            if z < radius:
-                output += "#"
-            else:
-                output += " "
-        output += "\n"
     return output
 
 radius = int(raw_input("Insert radius"))
--- src/graded/figures/IsoscelesTriangle.py
+++ src/graded/figures/IsoscelesTriangle.py
@@ -1,11 +1,26 @@
-def build_isosceles_triangle(figure, amountRows):
+def iso_triangle(figure, amountRows):
     output = ""
+    figure_record = ""
 
-    # TODO
+    # we start at 1 as we have only one asterisk at the top
+    offset = 1
+    destination = (amountRows + 1)
+
+    for row in range(offset, destination):
+        # calculate the amount of spaces to include from the left side based on the current row
+        amt_spaces = (amountRows - row)
+        for col in range(amt_spaces):
+            output += " "
+
+        # Adds two figures to the output for every row that comes after the first (top to bottom)
+        output += figure_record + figure_record + figure
+        figure_record += figure
+
+        output += "\n"
 
     return output
 
 figure = raw_input("Insert figure")
 amountRows = int(raw_input("Insert amount rows"))
 
-print build_isosceles_triangle(figure, amountRows)
+print iso_triangle(figure, amountRows)
